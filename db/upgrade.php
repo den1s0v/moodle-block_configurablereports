bc32b910dbd683f19e6219198210277064d75d6b
den1s0v <hello-lumen@ya.ru>
2026-07-15 01:53:36 +0300

    revert: useless no-op savepoint

--- db/upgrade.php
+++ db/upgrade.php
@@ -259,9 +259,5 @@ function xmldb_block_configurable_reports_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2027061418, 'block', 'configurable_reports');
     }
 
-    if ($oldversion < 2027061419) {
-        upgrade_plugin_savepoint(true, 2027061419, 'block', 'configurable_reports');
-    }
-
     return true;
 }
